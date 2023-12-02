cfafc12339e2cda206c8efe75c66b83ff4d66608
Magdiel Carreno <magdiel.carreno@latam.com>
2023-12-02 14:24:31 -0300

    mi primera subida

---
+++ series_asignacio serie jugador.sql
@@ -0,0 +1,8 @@
+INSERT INTO series (id,serie,created_at,updated_at) VALUES
+	 (1,'TERCERA','',''),
+	 (2,'SEGUNDA','',''),
+	 (3,'SENIOR35','',''),
+	 (4,'INFANTIL','',''),
+	 (5,'FEMENINO','',''),
+	 (6,'SENIOR40','',''),
+	 (7,'PRIMERA','','');
