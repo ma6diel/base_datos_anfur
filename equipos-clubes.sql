99e699691a12a81e791d3575e388f18662c73e4a
Magdiel Carreno <magdiel.carreno@latam.com>
2023-12-02 18:10:19 -0300

    primera subida de la pagina

--- equipos-clubes.sql
+++ equipos-clubes.sql
@@ -1,20 +1,20 @@
 INSERT INTO equipos (id,nombre,fecha_fundacion,cancha,presidente,secretario,created_at,updated_at,`url`,logo) VALUES
-	 (1,'Ind. Cuncumen','2023-11-01','Cancha de Cerrillos','x','x','2023-10-31 23:36:15','2023-11-14 02:35:02','wwww.test.cl','public/images/test.png'),
-	 (2,'El Paico','2023-11-01','Cancha el Paico','x','x','2023-11-01 01:35:18','2023-11-01 02:41:17','wwww.test.cl','public/images/test.png'),
-	 (3,'D. Baquedano','2023-11-01','Cancha el Transito','x','x','2023-11-01 01:53:27','2023-11-01 02:40:51','wwww.test.cl','public/images/test.png'),
-	 (5,'Cerrillo','2023-11-17','Cancha de Cerrillos','x','x','2023-11-01 02:33:27','2023-11-12 01:41:10','wwww.test.cl','public/images/test.png'),
-	 (6,'Viña Pomaire','2023-11-01','Estadio de Pomaire','x','x','2023-11-01 02:34:02','2023-11-01 02:34:02','wwww.test.cl','public/images/test.png'),
-	 (7,'Chiñigue los Quilos','2023-11-01','Cancha Los quilos','x','x','2023-11-01 02:34:44','2023-11-01 02:34:44','wwww.test.cl','public/images/test.png'),
-	 (8,'El transito','2023-11-01','Cancha el Transito','x','x','2023-11-01 02:35:12','2023-11-01 02:35:12','wwww.test.cl','public/images/test.png'),
-	 (9,'Estrella del Asilo','2023-11-01','Cancha Esmeralda 1','x','x','2023-11-01 02:35:54','2023-11-01 02:35:54','wwww.test.cl','public/images/test.png'),
-	 (10,'Estrella de Rumay','2023-11-10','Estadio de  Rumay','x','x','2023-11-01 02:37:35','2023-11-24 04:52:49','wwww.test.cl','public/images/test.png');
+	 (1,'Ind. Cuncumen','2023-11-01','Cancha de Cerrillos','x','x','2023-10-31 23:36:15','2023-11-14 02:35:02','https://maps.google.com?q=Cancha%20Cerrillos%20de%20Puangue,%20Melipilla,%20Regi%C3%B3n%20Metropolitana&ftid=0x9662514508369bdf:0xc1ed01bf4cffae40&hl=es-CL&gl=cl&entry=gps&lucs=,47071704','images/cuncumen.png'),
+	 (2,'El Paico','2023-11-01','Cancha el Paico','x','x','2023-11-01 01:35:18','2023-11-01 02:41:17','https://maps.app.goo.gl/t7KsRxW8ZvRLvbF96','images/paico.png'),
+	 (3,'D. Baquedano','2023-11-01','Cancha el Transito','x','x','2023-11-01 01:53:27','2023-11-01 02:40:51','https://maps.google.com/?q=-33.639587,-71.189835','images/baquedano.png'),
+	 (5,'Cerrillo','2023-11-17','Cancha de Cerrillos','x','x','2023-11-01 02:33:27','2023-11-12 01:41:10','https://maps.google.com?q=Cancha%20Cerrillos%20de%20Puangue,%20Melipilla,%20Regi%C3%B3n%20Metropolitana&ftid=0x9662514508369bdf:0xc1ed01bf4cffae40&hl=es-CL&gl=cl&entry=gps&lucs=,47071704','images/cerrillo.png'),
+	 (6,'Viña Pomaire','2023-11-01','Estadio de Pomaire','x','x','2023-11-01 02:34:02','2023-11-01 02:34:02','https://waze.com/ul/h66j2sckk5','images/viñapomaire.png'),
+	 (7,'Chiñigue los Quilos','2023-11-01','Cancha Los quilos','x','x','2023-11-01 02:34:44','2023-11-01 02:34:44','wwww.test.cl','images/losquilos.png'),
+	 (8,'El transito','2023-11-01','Cancha el Transito','x','x','2023-11-01 02:35:12','2023-11-01 02:35:12','https://maps.google.com/?q=-33.639587,-71.189835','images/transito.png'),
+	 (9,'Estrella del Asilo','2023-11-01','Cancha Esmeralda 1','x','x','2023-11-01 02:35:54','2023-11-01 02:35:54','wwww.test.cl','images/asilo.png'),
+	 (10,'Estrella de Rumay','2023-11-10','Estadio de  Rumay','x','x','2023-11-01 02:37:35','2023-11-24 04:52:49','wwww.test.cl','images/rumay.png');
 INSERT INTO equipos (id,nombre,fecha_fundacion,cancha,presidente,secretario,created_at,updated_at,`url`,logo) VALUES
-	 (11,'Deportivo Leyda','2023-11-01','Cancha Lumbrera','x','x','2023-11-01 02:38:16','2023-11-01 02:38:16','wwww.test.cl','public/images/test.png'),
-	 (12,'Real Santa Rosa','2023-11-01','Cancha Esmeralda 1','x','x','2023-11-01 02:39:03','2023-11-01 02:39:03','wwww.test.cl','public/images/test.png'),
-	 (13,'Union Chile','2023-11-01','Cancha Filomena','x','x','2023-11-01 02:39:31','2023-11-01 02:39:31','wwww.test.cl','public/images/test.png'),
-	 (14,'Rosa Elvira Matte','2023-11-01','Cancha Rosa Elvira','x','x','2023-11-01 02:40:12','2023-11-01 02:40:12','wwww.test.cl','public/images/test.png'),
-	 (15,'Union Esmeralda','2023-11-01','Cancha Esmeralda 2','x','x','2023-11-01 02:41:57','2023-11-01 02:41:57','wwww.test.cl','public/images/test.png'),
-	 (16,'D. Pomaire','2023-11-01','Cancha de Pomaire','x','x','2023-11-01 02:42:58','2023-11-01 02:42:58','wwww.test.cl','public/images/test.png'),
-	 (17,'Libertad de Bollenar','2023-11-01','Cancha de Libertad','x','x','2023-11-01 02:43:18','2023-11-01 02:43:18','wwww.test.cl','public/images/test.png'),
-	 (18,'Union San Jose','2023-11-01','Cancha de Libertad','x','x','2023-11-01 02:43:48','2023-11-01 02:43:48','wwww.test.cl','public/images/test.png');
+	 (11,'Deportivo Leyda','2023-11-01','Cancha Lumbrera','x','x','2023-11-01 02:38:16','2023-11-01 02:38:16','https://maps.app.goo.gl/JvrvmBf9F4cGUctD9?g_st=iw','images/leyda.png'),
+	 (12,'Real Santa Rosa','2023-11-01','Cancha Esmeralda 1','x','x','2023-11-01 02:39:03','2023-11-01 02:39:03','wwww.test.cl','images/test.png'),
+	 (13,'Union Chile','2023-11-01','Cancha Filomena','x','x','2023-11-01 02:39:31','2023-11-01 02:39:31','https://maps.app.goo.gl/XGvZjxCzW6SMugpQ8','images/unionchile.png'),
+	 (14,'Rosa Elvira Matte','2023-11-01','Cancha Rosa Elvira','x','x','2023-11-01 02:40:12','2023-11-01 02:40:12','https://maps.app.goo.gl/76o1T6Z4DZHfn2mB8','images/rem.png'),
+	 (15,'Union Esmeralda','2023-11-01','Cancha Esmeralda 2','x','x','2023-11-01 02:41:57','2023-11-01 02:41:57','https://maps.app.goo.gl/RF1AehRHuixLNrn68','images/esmeralda.png'),
+	 (16,'D. Pomaire','2023-11-01','Cancha de Pomaire','x','x','2023-11-01 02:42:58','2023-11-01 02:42:58','wwww.test.cl','images/dpomaire.png'),
+	 (17,'Libertad de Bollenar','2023-11-01','Cancha de Libertad','x','x','2023-11-01 02:43:18','2023-11-01 02:43:18','https://maps.app.goo.gl/91GL8odUBmwqUfEK8','images/libertad.png'),
+	 (18,'Union San Jose','2023-11-01','Cancha de Libertad','x','x','2023-11-01 02:43:48','2023-11-01 02:43:48','wwww.test.cl','images/sanjose.png');
 
